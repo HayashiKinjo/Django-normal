f16fd3354c1c1f849d2213cff9c656ec9062a580
Alvin Lim <alvin@jewelpaymentech.com>
2016-04-13 14:53:09 +0800

    Tutorial05 end- with 2 test cases failed.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here
 
@@ -19,7 +20,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
